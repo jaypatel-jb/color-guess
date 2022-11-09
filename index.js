86b5011e1ea8e001f5c282616f026859d5c25633
jay patel <pateljau2401@gmail.com>
2022-11-09 16:02:24 +0530

    remove border in *

---
+++ index.js
@@ -0,0 +1,45 @@
+let boxs = document.getElementById("cbox")
+let box = document.querySelectorAll(".box")
+let btn = document.getElementById("Reset")
+let rgbtext = document.getElementById("st");
+let gamebox = document.querySelector(".hbox")
+let a;
+
+btn.addEventListener("click", function () {
+  let rbox = Math.floor(Math.random() * box.length);
+
+  let a = one();
+  afilter = a.replace(/\s+/g, '');
+
+  gamebox.style.backgroundColor = afilter
+
+
+  rgbtext.innerText = afilter
+  for (let i = 0; i < box.length; i++) {
+    let random_val = one();
+
+    box[i].style.backgroundColor = `${random_val}`;
+    box[i].addEventListener("click", checkColor);
+  }
+  let k = box[rbox].style.backgroundColor = afilter
+})
+
+function xyz() {
+  return Math.floor(Math.random() * 250)
+}
+
+function one() {
+  return `rgb(${xyz()},${xyz()},${xyz()})`;
+}
+
+function checkColor(e) {
+  let bg = this.style.backgroundColor;
+  bg = bg.replace(/\s+/g, '');
+  if (bg == afilter) {
+    box.forEach((item) => {
+      item.style.backgroundColor = bg;
+    })
+  } else {
+    this.style.backgroundColor = 'transparent'
+  }
+}
